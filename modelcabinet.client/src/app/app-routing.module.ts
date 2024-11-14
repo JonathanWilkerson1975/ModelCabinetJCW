466e1486a6f7c24611502e85270da98cff2fffcf
Clarissa Lopez <lopezclarissa2021@gmail.com>
2024-11-14 14:05:34 -0800

    got the landing page connected to the route

--- modelcabinet.client/src/app/app-routing.module.ts
+++ modelcabinet.client/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 // TODO: Change Names to actual Module Names
 const routes: Routes = [
   { path: '', pathMatch: "full", component: LandingPageComponent },
-  { path: 'Projects', component: ProjectListPageComponent },
-  { path: 'Projects/:id', component: ProjectPageComponent },
-  { path: 'Help', component: HelpPageComponent },
-  { path: 'User', component: UserPageComponent }
+  { path: 'LandingPage', redirectTo: '' },
+  // { path: 'Projects', component: ProjectListPageComponent },
+  // { path: 'Projects/:id', component: ProjectPageComponent },
+  // { path: 'Help', component: HelpPageComponent },
+  // { path: 'User', component: UserPageComponent }
 ];
 
 @NgModule({
