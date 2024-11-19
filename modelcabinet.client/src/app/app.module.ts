9f193aa70fba44bb90776bd5db13947d4525c38f
gotsanity <gotsanity@gmail.com>
2024-11-19 12:50:08 -0800

    Update app.module.ts to include missing nav-bar component reference.

--- modelcabinet.client/src/app/app.module.ts
+++ modelcabinet.client/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
