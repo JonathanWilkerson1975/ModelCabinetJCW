7f60ce7bf082f8fd84316f44215c5ba36b7a89e2
Jesse Harlan <gotsanity@gmail.com>
2024-11-19 17:48:47 -0800

    updated workflows to seperate linting, added default lines to TS models for assets and projects and updated app.component.ts to remove lint.

--- modelcabinet.client/src/app/Models/asset.ts
+++ modelcabinet.client/src/app/Models/asset.ts
@@ -1,3 +1,3 @@
 export interface Asset {
-
+    deleteMe: string;
 }
--- modelcabinet.client/src/app/app.component.ts
+++ modelcabinet.client/src/app/app.component.ts
@@ -1,13 +1,8 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-
-  constructor() {}
-
-}
+export class AppComponent {}
