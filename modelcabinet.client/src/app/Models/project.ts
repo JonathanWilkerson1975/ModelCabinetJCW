34f04e22c14ae81033962d2b5d1268b0821da38f
Connor Gaskill <gaskillconnor@gmail.com>
2024-11-20 11:57:05 -0800

    Added and updated project model on backend and frontend

--- modelcabinet.client/src/app/Models/project.ts
+++ modelcabinet.client/src/app/Models/project.ts
@@ -1,3 +1,18 @@
+import { Asset } from "./asset";
+
+export interface projectAsset {
+  $values: Asset[]
+}
 export interface Project {
+
+  projectID: number,
+  name: String,
+  creationDate: Date,
+  modifiedDate: Date,
+  description: String,
+  author: String,
+  version: String,
+  assets: projectAsset,
+  shortDescription: String,
 
 }
