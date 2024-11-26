57e92beaddb17d39ad5620a14a2783a2426be62a
jonathan wilkerson <jonathan.wilkerson@student.centralia.edu>
2024-11-26 11:49:38 -0800

    Chnage On Navbar Help PAge upload

--- modelcabinet.client/src/app/Help-Page/Help-Page.component.spec.ts
+++ modelcabinet.client/src/app/Help-Page/Help-Page.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AboutProjectComponent } from './Help-Page.component';
+import { HelpProjectComponent } from './Help-Page.component';
 
-describe('AboutProjectComponent', () => {
-  let component: AboutProjectComponent;
-  let fixture: ComponentFixture<AboutProjectComponent>;
+describe('HelpProjectComponent', () => {
+  let component: HelpProjectComponent;
+  let fixture: ComponentFixture<HelpProjectComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AboutProjectComponent]
+      declarations: [HelpProjectComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(AboutProjectComponent);
+    fixture = TestBed.createComponent(HelpProjectComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- modelcabinet.client/src/app/app-routing.module.ts
+++ modelcabinet.client/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ProjectListPageComponent } from './projects/project-list-page/project-l
 import { ProjectPageComponent } from './projects/project-page/project-page.component';
 import { AboutProjectComponent } from './about-project/about-project.component';
 import { ChangelogComponent } from './changelog/changelog.component'
+import { HelpProjectComponent } from './Help-Page/Help-Page.component';
 
 // TODO: Change Names to actual Module Names
 const routes: Routes = [
@@ -13,9 +14,9 @@ const routes: Routes = [
   { path: 'Projects', component: ProjectListPageComponent },
   { path: 'Projects/:id', component: ProjectPageComponent },
   { path: 'about-project', component: AboutProjectComponent },
-  { path: 'changelog', component: ChangelogComponent}
-  // { path: 'Help', component: HelpPageComponent },
-  // { path: 'User', component: UserPageComponent }
+  { path: 'changelog', component: ChangelogComponent},
+  { path: 'Help', component: HelpProjectComponent }
+  /* { path: 'User', component: UserPageComponent }*/
 ];
 
 @NgModule({
