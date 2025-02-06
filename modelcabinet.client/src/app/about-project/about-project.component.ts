2c518e68fb0c699dcf6096a2a08a5948a020ce56
Brittany Hancock <femaleentp@gmail.com>
2025-02-06 07:21:42 -0800

    Added dynamic GitHub contributors to About page

--- modelcabinet.client/src/app/about-project/about-project.component.ts
+++ modelcabinet.client/src/app/about-project/about-project.component.ts
@@ -6,18 +6,4 @@ import { Component } from '@angular/core';
   styleUrl: './about-project.component.css'
 })
 export class AboutProjectComponent {
-  developers = [
-    { name: 'Brittany H', image: 'assets/images/dev_placeholder.png' },
-    { name: 'Clarissa L', image: 'assets/images/dev_placeholder.png' },
-    { name: 'Colton S', image: 'assets/images/dev_placeholder.png' },
-    { name: 'Connor G', image: 'assets/images/dev_placeholder.png' },
-    { name: 'Jesse H', image: 'assets/images/dev_placeholder.png' },
-    { name: 'Jonathan W', image: 'assets/images/dev_placeholder.png' },
-    { name: 'Jude M', image: 'assets/images/dev_placeholder.png' },
-    { name: 'Juliea R', image: 'assets/images/dev_placeholder.png' },
-    { name: 'Kathy Y', image: 'assets/images/dev_placeholder.png' },
-    { name: 'Khai H', image: 'assets/images/dev_placeholder.png' },
-    { name: 'Lee H', image: 'assets/images/dev_placeholder.png' },
-    { name: 'Sarah H', image: 'assets/images/dev_placeholder.png' },
-    { name: 'William R', image: 'assets/images/dev_placeholder.png' }];
 }
