b9d2dcb94a2e68a5d143b593e61674d4a977f83d
MagShadowLight <williamrose1025@icloud.com>
2025-02-11 14:15:16 -0800

    Added CustomPipe for file size format

---
+++ modelcabinet.client/src/app/Pipes/filesize.pipe.ts
@@ -0,0 +1,30 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'fileSize',
+  pure: false
+})
+export class FileSizePipe implements PipeTransform {
+  transform(filesize: number): string {
+    const byteSizes: number[] = [1000000000000000, 1000000000000, 1000000000, 1000000, 1000];
+    //PB
+    if (filesize >= byteSizes[0]) {
+      return `${filesize / byteSizes[0]} PB`;
+    } //TB
+    else if (filesize >= byteSizes[1]) {
+      return `${filesize / byteSizes[1]} TB`;
+    } //GB
+    else if (filesize >= byteSizes[2]) {
+      return `${filesize / byteSizes[2]} GB`;
+    } //MB
+    else if (filesize >= byteSizes[3]) {
+      return `${filesize / byteSizes[3]} MB`;
+    } //KB
+    else if (filesize >= byteSizes[4]) {
+      return `${filesize / byteSizes[4]} KB`;
+    } //B
+    else {
+      return `${filesize} B`;
+    }
+  }
+}
