54d11cf81cb72dffd5be690c1c5c330d6fb0d057
MagShadowLight <williamrose1025@icloud.com>
2025-02-17 09:40:37 -0800

    changes to the filesizepipe so it only show one decimal point to display.

--- modelcabinet.client/src/app/Pipes/file-size.pipe.ts
+++ modelcabinet.client/src/app/Pipes/file-size.pipe.ts
@@ -9,19 +9,19 @@ export class FileSizePipe implements PipeTransform {
     const byteSizes: number[] = [1000000000000000, 1000000000000, 1000000000, 1000000, 1000];
     //PB
     if (filesize >= byteSizes[0]) {
-      return `${filesize / byteSizes[0]} PB`;
+      return `${(filesize / byteSizes[0]).toFixed(1)} PB`;
     } //TB
     else if (filesize >= byteSizes[1]) {
-      return `${filesize / byteSizes[1]} TB`;
+      return `${(filesize / byteSizes[1]).toFixed(1)} TB`;
     } //GB
     else if (filesize >= byteSizes[2]) {
-      return `${filesize / byteSizes[2]} GB`;
+      return `${(filesize / byteSizes[2]).toFixed(1)} GB`;
     } //MB
     else if (filesize >= byteSizes[3]) {
-      return `${filesize / byteSizes[3]} MB`;
+      return `${(filesize / byteSizes[3]).toFixed(1)} MB`;
     } //KB
     else if (filesize >= byteSizes[4]) {
-      return `${filesize / byteSizes[4]} KB`;
+      return `${(filesize / byteSizes[4]).toFixed(1)} KB`;
     } //B
     else {
       return `${filesize} B`;
