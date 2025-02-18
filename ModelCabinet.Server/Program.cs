912304a23adb82b8a50e6d575e8ca3c0b37e576f
Nartzes <Khaiha265@gmail.com>
2025-02-17 16:45:12 -0800

    +Added auto switch on launch mas and dev

--- ModelCabinet.Server/Program.cs
+++ ModelCabinet.Server/Program.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ModelCabinet.Server.Data;
+
 
 namespace ModelCabinet.Server
 {
@@ -8,39 +10,98 @@ namespace ModelCabinet.Server
     {
         public static void Main(string[] args)
         {
+
+
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddDbContext<ModelCabinetContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("ModelCabinetContext") ?? throw new InvalidOperationException("Connection string 'ModelCabinetContext' not found.")));
+                options.UseSqlServer(builder.Configuration.GetConnectionString("ModelCabinetContext")
+                    ?? throw new InvalidOperationException("Connection string 'ModelCabinetContext' not found.")));
+
 
             // Add services to the container.
-
-            builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+
             var app = builder.Build();
+
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
 
-            // Configure the HTTP request pipeline.
+
             if (app.Environment.IsDevelopment())
             {
+#if DEBUG
+                Console.WriteLine("Running in DEBUG mode - Switching to 'development' branch.");
+                SwitchToCorrectBranch("development");
+#else
+                    Console.WriteLine("Running in RELEASE mode - Ensuring 'master' branch is used.");
+                    SwitchToCorrectBranch("master");
+#endif
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+            else
+            {
+
+
+            }
+
+
+            // Configure the HTTP request pipeline.
+
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
-
             app.MapFallbackToFile("/index.html");
+
 
             app.Run();
         }
+
+
+        private static void SwitchToCorrectBranch(string branchName)
+        {
+            try
+            {
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = "git",
+                    Arguments = $"switch {branchName}",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+
+                using (Process process = new Process { StartInfo = psi })
+                {
+                    process.Start();
+                    string output = process.StandardOutput.ReadToEnd();
+                    string error = process.StandardError.ReadToEnd();
+                    process.WaitForExit();
+
+
+                    Console.WriteLine($"Git Output: {output}");
+                    if (!string.IsNullOrEmpty(error))
+                    {
+                        Console.WriteLine($"Git Error: {error}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error switching to {branchName} branch: {ex.Message}");
+            }
+        }
     }
 }
+
+
+
+
