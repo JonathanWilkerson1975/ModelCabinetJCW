20b0bff29a48dbdb47f0338566d69bcb577be09f
gotsanity <gotsanity@gmail.com>
2025-02-19 23:30:03 -0800

    added example of mock service

--- modelcabinet.client/src/app/asset/asset-detail/asset-detail.component.spec.ts
+++ modelcabinet.client/src/app/asset/asset-detail/asset-detail.component.spec.ts
@@ -4,20 +4,28 @@ import { AssetDetailComponent } from './asset-detail.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { FileSizePipe } from '../../Pipes/file-size.pipe';
+import { DataService } from '../../data.service';
 
 describe('AssetDetailComponent', () => {
   let component: AssetDetailComponent;
   let fixture: ComponentFixture<AssetDetailComponent>;
 
-  // TODO: fix for spying on data object - let spyData = jasmine.createSpyObj('DataService', ['getAllAssets']);
+  // create the mock service
+  let mockDataService = jasmine.createSpyObj(['getAllAssets']);
 
   beforeEach(async () => {
     // global arrange
+
+    // To specify the value returned by a mock service:
+    // mockDataService.getAllAssets.and.returnValue('toast');
+
     await TestBed.configureTestingModule({
       declarations: [
         AssetDetailComponent,
         FileSizePipe,
-        
+      ],
+      providers: [
+        { provide: DataService, useValue: mockDataService }
       ],
       imports: [
         RouterTestingModule.withRoutes([]),
@@ -36,6 +44,11 @@ describe('AssetDetailComponent', () => {
   it('should create', () => {
     // arrange more
     expect(component).toBeTruthy();
+  });
+
+  it('should call DataService.getAllAssets()', () => {
+    expect(mockDataService.getAllAssets).toHaveBeenCalled();
+    // expect(component.dataGatheredByMockService).toEqual('blah');
   });
 
 });
