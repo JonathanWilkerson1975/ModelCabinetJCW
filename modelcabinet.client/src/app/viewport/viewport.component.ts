8285c97a1db2630c022d8cb4f2944f56d797a10d
Jesse Harlan <jesse.harlan@centralia.edu>
2025-02-27 13:50:52 -0800

    Assets now render in the viewport

--- modelcabinet.client/src/app/viewport/viewport.component.ts
+++ modelcabinet.client/src/app/viewport/viewport.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, Input, NgZone, OnInit } from '@angular/core';
+import { Asset } from '../Models/asset';
+import { StlModelViewerComponent } from 'angular-stl-model-viewer';
+
+// @ts-ignore
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-viewport',
   templateUrl: './viewport.component.html',
-  styleUrl: './viewport.component.css'
+  styleUrl: './viewport.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ViewportComponent {
+export class ViewportComponent extends StlModelViewerComponent implements OnInit {
+  constructor(private _cdr: ChangeDetectorRef, private _eleRef: ElementRef, private _ngZone: NgZone) {
+    super(_cdr, _eleRef, _ngZone);
+    
+  }
 
 }
