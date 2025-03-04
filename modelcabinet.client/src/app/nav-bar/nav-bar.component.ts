c7c6e0de1c63ab234bf2de42fd3b130931fc018e
Clarissa Lopez <lopezclarissa2021@gmail.com>
2025-03-04 13:21:59 -0800

    adding logout to navbar.

--- modelcabinet.client/src/app/nav-bar/nav-bar.component.ts
+++ modelcabinet.client/src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,4 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
+  constructor(private authService: AuthService) { }
+
+  isAuthenticated(): boolean {
+    return this.authService.isAuthenticated(); 
+  }
+
+  logout(): void {
+    this.authService.logout();
+    window.location.reload(); 
+  }
 }
